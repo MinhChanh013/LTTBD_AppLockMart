d5093e611d04d12a31b295db92942900e0777db8
Nguyen Minh Chanh <nguyenminhchanh1910@gmail.com>
2022-11-12 10:45:20 +0700

    ton tet

---
+++ src/page/main/components/Product_Detail/Product_Detail.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Product_Detail() {
+  return (
+    <View>
+      <Text>Product_Detail</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/page/main/components/Shopping_Cart/Shopping_Cart.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Shopping_Cart() {
+  return (
+    <View>
+      <Text>Shopping_Cart</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
